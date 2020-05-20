642535919cc8479a9e58ce6eea1a10508def98bc
Khalid Williams <khalidwill27@gmail.com>
2020-05-19 21:46:10 -0600

    Add Error route

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import MainView from "../MainView/MainView";
-import { Route, Link } from "react-router-dom";
+import { Switch, Redirect, Route, Link } from "react-router-dom";
 import PostView from "../PostView/PostView";
+import Error from '../Error/Error';
 
 class App extends Component {
   constructor() {
@@ -80,35 +81,42 @@ class App extends Component {
         <h1 className="heading">
           <Link to="/">PostIt</Link>
         </h1>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <MainView
-              addPost={this.addPost}
-              changeScore={this.changeScore}
-              posts={this.state.posts}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/posts/:id"
-          render={({ match }) => {
-            //console.log(match.params.id)
-            const postToRender = this.state.posts.find(
-              post => post.id === parseInt(match.params.id)
-            );
-            //console.log(postToRender)
-            return (
-              <PostView
-                post={postToRender}
+        <Switch>
+          <Route
+            exact
+            path="/posts/:id"
+            render={({ match }) => {
+              //console.log(match.params.id)
+              const postToRender = this.state.posts.find(
+                post => post.id === parseInt(match.params.id)
+              );
+              //console.log(postToRender)
+              return (
+                <PostView
+                  post={postToRender}
+                  changeScore={this.changeScore}
+                  addComment={this.addComment}
+                />
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <MainView
+                addPost={this.addPost}
                 changeScore={this.changeScore}
-                addComment={this.addComment}
+                posts={this.state.posts}
               />
-            );
-          }}
-        />
+            )}
+          />
+          <Route
+            path="/error"
+            render={() => <Error />}
+          />
+          <Redirect to="/error"/>
+        </Switch>
       </>
     );
   }
